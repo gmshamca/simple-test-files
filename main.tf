d1ebcc9e896c268f2411256ca2f8c90701315953
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-06-25 14:29:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
-resource  "build-trigger" "buil-run" {
+resource  "local_file" "buil-run" {
   
   repo_type = var.repo_type
   git_repo  = var.git_repo
   branch    = "main"
     
 }
+
+provider "google" {
+  project      = "test-project"
+  region       = var.region
+}
+
   
