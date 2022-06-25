599916e555f1b3e192ae76a332dc59900af466b1
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-06-25 14:08:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
-module "build-trigger" {
-  for_each  = var.git_repo
-  source               = "../../modules/cloud_build_trigger_run"
-  name      = each.value.name
-  uri       = each.value.uri
-  branch    = "main"
+resourece  "build-trigger" "buil-run" {
+  
   repo_type = var.repo_type
-  imagename = each.value.imagename
-   
+  git_repo  = var.git_repo
+  branch    = "main"
+    
 }
   
